32a6565775bcdad0ac51de2cff1f04c8d12de317
Christopher Zenner <christopher.zenner@gmail.com>
2020-04-20 17:03:20 -0700

    feat: add functionality to read CLI input
    - Install Lodash.
    - Create functionality to read and store user input from the command
      line.

--- liri.js
+++ liri.js
@@ -1,4 +1,24 @@
 require('dotenv').config();
 
-const keys = require('./keys.js'),
-      spotify = new Spotify(keys.spotify);
+const _ = require('lodash'),
+      keys = require('./keys.js'),
+      // spotify = new Spotify(keys.spotify),
+      usrCmd = process.argv[2],
+      searchTerm = process.argv.slice(3);
+
+switch (usrCmd) {
+  case 'concert-this':
+    console.log(`You want to ${usrCmd} for ${searchTerm.join(' ')}!`);
+    break;
+  case 'movie-this':
+    console.log(`You want to ${usrCmd} for ${searchTerm.join(' ')}!`);
+    break;
+  case 'spotify-this-song':
+    console.log(`You want to ${usrCmd} for ${searchTerm.join(' ')}!`);
+    break;
+  case 'do-what-it-says':
+    console.log(`You want to ${usrCmd} for ${searchTerm.join(' ')}!`);
+    break;
+  default:
+    console.log('BAD USER! Invalid command entered.');
+};
