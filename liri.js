bc84ab1fd9b840e4806bddf75184d5cb03f7103d
Christopher Zenner <christopher.zenner@gmail.com>
2020-04-20 17:37:16 -0700

    feat: query concert data upon user request
    When user inputs `concert-this`, query Bandsintown API for concert info
    on the specified artist.

--- liri.js
+++ liri.js
@@ -1,14 +1,59 @@
 require('dotenv').config();
 
 const _ = require('lodash'),
+      axios = require('axios'),
+      moment = require('moment'),
       keys = require('./keys.js'),
       // spotify = new Spotify(keys.spotify),
       usrCmd = process.argv[2],
-      searchTerm = process.argv.slice(3);
+      searchTerms = process.argv.slice(3);
 
 switch (usrCmd) {
   case 'concert-this':
-    console.log(`You want to ${usrCmd} for ${searchTerm.join(' ')}!`);
+    // ASSERT: User wants to see concert info for a particular artist.
+    
+    // Format artist's name for query URL and output to user.
+    let artist = '', 
+        artistProperName = '';
+
+    _.forEach(searchTerms, (word) => {
+      artist += `${_.lowerCase(word)}+`;
+      artistProperName += _.upperFirst(word) + ' ';
+    });
+
+    artist = _.trimEnd(artist, '+');
+    artistProperName = _.trimEnd(artistProperName, ' ');
+    
+    // Create query URL, and get data from Bandsintown
+    const queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+
+    axios.get(queryURL)
+      .then((res) => {
+        // DEBUG:
+        // console.log(res);
+
+        const concertData = res.data;        
+        let concertInfo = [];
+
+        _.forEach(concertData, (concert) => {
+          concertInfo.push(
+              '--------------------------------------------------\n' +
+              `Concert Venue: ${concert.venue.name}\n` +
+              `Venue Location: ${concert.venue.location}\n` +
+              `Concert Date: ${moment(concert.datetime).format('MM/DD/YYYY')}`
+            );
+        });
+
+        concertInfo.push('--------------------------------------------------');
+
+        console.log(`${artistProperName} has the following concerts coming up:\n`);
+
+        _.forEach(concertInfo, (concert) => {
+          console.log(concert);
+        })
+      })
+      .catch(console.error);
+
     break;
   case 'movie-this':
     console.log(`You want to ${usrCmd} for ${searchTerm.join(' ')}!`);
