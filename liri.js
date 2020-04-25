0e8f61d1779afc75d88165a98a9479c7512a3944
Christopher Zenner <christopher.zenner@gmail.com>
2020-04-25 01:42:05 -0700

    refactor: abstract `concert-this` functionality
    Encapsulate `concert-this` query and output functionality in its own
    function.

--- liri.js
+++ liri.js
@@ -12,73 +12,76 @@ const _ = require('lodash'),
 
 let queryURL = '';
 
+function concertThis() {
+  // Format artist's name for query URL and output to user.
+  let artist = '', 
+      artistProperName = '';
+
+  _.forEach(searchTerms, (word) => {
+    let partialName = _.lowerCase(word);
+
+    artist += `${partialName}+`;
+    artistProperName += _.upperFirst(partialName) + ' ';
+  });
+
+  artist = _.trimEnd(artist, '+');
+  artistProperName = _.trimEnd(artistProperName, ' ');
+  
+  // Create query URL, and get data from Bandsintown
+  queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+
+  axios.get(queryURL)
+    .then((res) => {
+      // DEBUG:
+      // console.log(res);
+
+      const concertData = res.data;        
+      let concertInfo = [];
+
+      _.forEach(concertData, (concert) => {
+        concertInfo.push(
+          '--------------------------------------------------\n' +
+          `Concert Venue: ${concert.venue.name}\n` +
+          `Venue Location: ${concert.venue.location}\n` +
+          `Concert Date: ${moment(concert.datetime).format('MM/DD/YYYY')}`
+        );
+      });
+
+      let userMsg = '';
+
+      if (concertInfo.length > 0) {
+        userMsg = `\n${artistProperName} has the following concerts coming up:\n`;
+        concertInfo.push('--------------------------------------------------\n');
+      }
+      else {
+        userMsg = `${artistProperName} has no concerts coming up. =[`
+      }
+
+      // Output result(s) to user.
+      console.log(userMsg);
+
+      _.forEach(concertInfo, (concert) => {
+        console.log(concert);
+      })
+
+      // Output result(s) to log file.
+      fs.appendFile('log.txt', 
+        `\n${userMsg}\n${concertInfo.join('\n')}`, 
+        (err) => {
+        if (err)
+          console.error(err);
+      });
+    })
+    .catch(console.error);
+}
+
 // DEBUG:
-console.log(`You want to ${usrCmd} for ${searchTerms.join(' ')}!`);
+// console.log(`You want to ${usrCmd} for ${searchTerms.join(' ')}!`);
 
 switch (usrCmd) {
   case 'concert-this':
     // ASSERT: User wants to see concert info for a particular artist.
-    
-    // Format artist's name for query URL and output to user.
-    let artist = '', 
-        artistProperName = '';
-
-    _.forEach(searchTerms, (word) => {
-      let partialName = _.lowerCase(word);
-
-      artist += `${partialName}+`;
-      artistProperName += _.upperFirst(partialName) + ' ';
-    });
-
-    artist = _.trimEnd(artist, '+');
-    artistProperName = _.trimEnd(artistProperName, ' ');
-    
-    // Create query URL, and get data from Bandsintown
-    queryURL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
-
-    axios.get(queryURL)
-      .then((res) => {
-        // DEBUG:
-        // console.log(res);
-
-        const concertData = res.data;        
-        let concertInfo = [];
-
-        _.forEach(concertData, (concert) => {
-          concertInfo.push(
-            '--------------------------------------------------\n' +
-            `Concert Venue: ${concert.venue.name}\n` +
-            `Venue Location: ${concert.venue.location}\n` +
-            `Concert Date: ${moment(concert.datetime).format('MM/DD/YYYY')}`
-          );
-        });
-
-        let userMsg = '';
-
-        if (concertInfo.length > 0) {
-          userMsg = `\n${artistProperName} has the following concerts coming up:\n`;
-          concertInfo.push('--------------------------------------------------\n');
-        }
-        else {
-          userMsg = `${artistProperName} has no concerts coming up. =[`
-        }
-
-        // Output result(s) to user.
-        console.log(userMsg);
-
-        _.forEach(concertInfo, (concert) => {
-          console.log(concert);
-        })
-
-        // Output result(s) to log file.
-        fs.appendFile('log.txt', 
-          `\n${userMsg}\n${concertInfo.join('\n')}`, 
-          (err) => {
-          if (err)
-            console.error(err);
-        });
-      })
-      .catch(console.error);
+    concertThis();
 
     break;
   case 'movie-this':
